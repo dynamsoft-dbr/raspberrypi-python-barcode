98c11b87834911da183216ec5454684cede22ec3
yushulx <lingxiao1002@gmail.com>
2017-12-18 08:26:57 +0800

    Update camera.py and camera_thread.py for DBR 5.2

--- camera.py
+++ camera.py
@@ -2,23 +2,6 @@ import cv2
 import dbr
 import time
 
-types = {
-        0x3FFL: "OneD",
-        0x1L  : "CODE_39",
-        0x2L  : "CODE_128",
-        0x4L  : "CODE_93",
-        0x8L  : "CODABAR",
-        0x10L : "ITF",
-        0x20L : "EAN_13",
-        0x40L : "EAN_8",
-        0x80L : "UPC_A",
-        0x100L: "UPC_E",
-        0x200L: "INDUSTRIAL_25",
-        0x2000000L: "PDF417",
-        0x8000000L: "DATAMATRIX",
-        0x4000000L: "QR_CODE"
-    }
-
 def get_time():
     localtime = time.localtime()
     capturetime = time.strftime("%Y%m%d%H%M%S", localtime)
@@ -32,7 +15,7 @@ def read_barcode():
     vc.set(4, 240) #set height
 
     if vc.isOpened(): # try to get the first frame
-        dbr.initLicense("B4E6216C77DC64BAB584DB7FF752B7D4")
+        dbr.initLicense("t0068MgAAAHtCgAPxEdCJN1bsu9n6YfnWDoaW7YZomIZZke2m9KynRnKSqsuQyd7Cdgo6razlb7VU3IFaKeBgg9Rq069Uihc=")
         rval, frame = vc.read()
     else:
         return
@@ -48,7 +31,7 @@ def read_barcode():
             print(get_time())
             print("Total count: " + str(len(results)))
             for result in results:
-                print("Type: " + types[result[0]])
+                print("Type: " + result[0])
                 print("Value: " + result[1] + "\n")
 
         # 'ESC' for quit
--- camera_thread.py
+++ camera_thread.py
@@ -4,23 +4,6 @@ import time
 import threading
 import Queue
 
-types = {
-        0x3FFL: "OneD",
-        0x1L  : "CODE_39",
-        0x2L  : "CODE_128",
-        0x4L  : "CODE_93",
-        0x8L  : "CODABAR",
-        0x10L : "ITF",
-        0x20L : "EAN_13",
-        0x40L : "EAN_8",
-        0x80L : "UPC_A",
-        0x100L: "UPC_E",
-        0x200L: "INDUSTRIAL_25",
-        0x2000000L: "PDF417",
-        0x8000000L: "DATAMATRIX",
-        0x4000000L: "QR_CODE"
-    }
-
 q = Queue.Queue(1)
 
 class BarcodeReaderThread (threading.Thread):
@@ -43,7 +26,7 @@ class BarcodeReaderThread (threading.Thread):
                 print(get_time())
                 print("Total count: " + str(len(results)))
                 for result in results:
-                    print("Type: " + types[result[0]])
+                    print("Type: " + result[0])
                     print("Value: " + result[1] + "\n")
 
         print("Quit thread")
@@ -60,7 +43,7 @@ def read_barcode():
     vc.set(4, 240) #set height
 
     if vc.isOpened(): # try to get the first frame
-        dbr.initLicense("B4E6216C77DC64BAB584DB7FF752B7D4")
+        dbr.initLicense("t0068MgAAAHtCgAPxEdCJN1bsu9n6YfnWDoaW7YZomIZZke2m9KynRnKSqsuQyd7Cdgo6razlb7VU3IFaKeBgg9Rq069Uihc=")
         rval, frame = vc.read()
     else:
         return
